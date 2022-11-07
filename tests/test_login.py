ae245e3e5f1ea76fd0be26384b935b17545e07ef
Igor Korobchenko <korobchenko.igor@gmail.com>
2022-11-07 09:21:06 -0800

    Run the First test case. Login process testing.

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,12 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.common.by import By
 
 o = webdriver.FirefoxOptions()
 o.headless = True
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=o)
+driver.get('https://www.saucedemo.com/')
 
-def test_open_main_page():
-    driver.get('https://www.saucedemo.com/')
-    assert driver.title == 'Swag Labs'
+def test_login():
+    driver.find_element(By.ID, 'user-name').send_keys('standard_user')
+    driver.find_element(By.ID, 'password').send_keys('secret_sauce')
+    driver.find_element(By.ID, 'login-button').click()
+    assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'We are on the product page. Test PASSED'
+
     driver.quit()

be1142279e38e4bed4e6411e66b56ab7cb53e688
Olesia Zhenirovska <oleszh@gmail.com>
2022-11-08 20:37:11 -0800

    second try

--- tests/test_login.py
+++ tests/test_login.py
@@ -34,7 +34,8 @@ o = webdriver.FirefoxOptions()
 o.headless = True
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=o)
 driver.get('https://www.saucedemo.com/')
-def test_login():
+def test_login_page():
+    print("Hello")
     driver.find_element(By.ID, 'user-name').send_keys('standard_user')
     driver.find_element(By.ID, 'password').send_keys('secret_sauce')
     driver.find_element(By.ID, 'login-button').click()

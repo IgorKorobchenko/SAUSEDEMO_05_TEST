a566d8ab216a12f60d0946f3eb5a1bd01ce4b82b
aharu <aharutyunyan2011@gmail.com>
2022-11-08 21:15:17 -0800

    Add my commit

--- tests/test_login.py
+++ tests/test_login.py
@@ -2,20 +2,26 @@
 #Expected result User should be directed to the page https://www.saucedemo.com/inventory.html
 
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
-o = webdriver.FirefoxOptions()
+o = webdriver.ChromeOptions()
 o.headless = True
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=o)
-driver.get('https://www.saucedemo.com/')
+driver = webdriver.Chrome(
+    service=ChromeService(ChromeDriverManager().install()), options=o
+)
+driver.get("https://www.saucedemo.com/")
+
 
 def test_login_page():
-    driver.find_element(By.ID, 'user-name').send_keys('standard_user')
-    driver.find_element(By.ID, 'password').send_keys('secret_sauce')
-    driver.find_element(By.ID, 'login-button').click()
-    assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'We are on the product page. Test PASSED'
+    driver.find_element(By.ID, "user-name").send_keys("standard_user")
+    driver.find_element(By.ID, "password").send_keys("secret_sauce")
+    driver.find_element(By.ID, "login-button").click()
+    assert (
+        driver.current_url == "https://www.saucedemo.com/inventory.html"
+    ), "We are on the product page. Test PASSED"
 
     driver.quit()
 #Hello
+#Add my commit

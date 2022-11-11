c36307b4645abb1c34dca2e7e956717a86ce6c9f
Diana Florea <diflorea15@gmail.com>
2022-11-10 20:00:04 -0800

    Filters click

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -5,11 +5,9 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 
 from webdriver_manager.firefox import GeckoDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
-options.headless = False
+options.headless = True
 driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
 driver.get('https://www.saucedemo.com/')
 
@@ -18,10 +16,10 @@ def test_login_page():
     driver.find_element(By.ID, 'password').send_keys('secret_sauce')
     driver.find_element(By.ID, 'login-button').click()
     assert driver.current_url == 'https://www.saucedemo.com/inventory.html', 'We are on the product page. Test PASSED'
-time.sleep(5)
+time.sleep(2)
 
 def test_filter():
     dropdown = driver.find_element(By.CLASS_NAME, 'product_sort_container')
     dropdown.click()
 
-time.sleep(5)
+time.sleep(2)

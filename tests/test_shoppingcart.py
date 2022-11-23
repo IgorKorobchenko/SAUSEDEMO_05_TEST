0ddbe6192e7d1d8cd8510e1a5230abb9fb122964
Igor Korobchenko <korobchenko.igor@gmail.com>
2022-11-22 20:39:40 -0800

    Shopping cart tests

--- tests/test_shoppingcart.py
+++ tests/test_shoppingcart.py
@@ -7,6 +7,7 @@ backpack_removal = (By.XPATH, "//div[@class='inventory_item_name']")
 # cart_quantity = (By.XPATH, "//div[@class='cart_quantity']")
 # shopping_cart_bage = (By.XPATH, "//span[@class='shopping_cart_badge']")
 inventory_url = 'https://www.saucedemo.com/inventory-item.html?id=4'
+shoppingcart_url = 'https://www.saucedemo.com/cart.html'
 
 #TC004.1 Check if the cart button is clickable
 def test_cart_button(d):
@@ -102,7 +103,40 @@ def test_checkout_form(d):
     title = d.find_element(By.CLASS_NAME, 'title').text
     assert title == 'CHECKOUT: OVERVIEW'
 
+# TC005.11 Check the CANCEL button
 
+def test_cancel_btn (d):
+    d.find_element(By.ID, "user-name").send_keys("standard_user")
+    d.find_element(By.ID, "password").send_keys("secret_sauce")
+    d.find_element(By.ID, "login-button").click()
+    d.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+    d.find_element(By.ID, "shopping_cart_container").click()
+    d.find_element(By.ID, "checkout").click()
+    d.find_element(By.ID, "first-name").send_keys('Maks')
+    d.find_element(By.ID, "last-name").send_keys('Plank')
+    d.find_element(By.ID, "postal-code").send_keys(91210)
+    d.find_element(By.ID, "continue").click()
+    d.find_element(By.ID, 'cancel').click()
+    title_catalog = d.find_element(By.CLASS_NAME, 'title').text
+    assert title_catalog == 'PRODUCTS'
+
+# TC005.12 Check the checkout overview page
+
+def test_checkout_page(d):
+    d.find_element(By.ID, "user-name").send_keys("standard_user")
+    d.find_element(By.ID, "password").send_keys("secret_sauce")
+    d.find_element(By.ID, "login-button").click()
+    d.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
+    d.find_element(By.ID, "shopping_cart_container").click()
+    d.find_element(By.ID, "checkout").click()
+    d.find_element(By.ID, "first-name").send_keys('Maks')
+    d.find_element(By.ID, "last-name").send_keys('Plank')
+    d.find_element(By.ID, "postal-code").send_keys(91210)
+    d.find_element(By.ID, "continue").click()
+    title_payment_info = d.find_element(By.CLASS_NAME, "summary_info_label").text
+    assert title_payment_info == 'Payment Information:'
+    title_shipping_info = d.find_element(By.CLASS_NAME, 'summary_info_label').text
+    assert title_shipping_info == 'Shipping Information:'
 
 
 
